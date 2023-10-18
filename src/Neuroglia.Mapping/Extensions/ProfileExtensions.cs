ce1e3829acbe1a8c5edf2b60764253ed351f195c
Charles d'Avernas <charles.davernas@neuroglia.io>
2023-10-18 10:25:09 +0200

    feat(Mapping): Added a new MapAttribute, used to mark the types an object is mappable to, as well as Profile extensions, used to automatically register mapped types and apply discovered IMappingConfigurations

--- src/Neuroglia.Mapping/Extensions/ProfileExtensions.cs
+++ src/Neuroglia.Mapping/Extensions/ProfileExtensions.cs
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Neuroglia.Mapping;
@@ -21,10 +22,7 @@ namespace Neuroglia.Mapping;
 public static class ProfileExtensions
 {
 
-    private static readonly MethodInfo GenericApplyConfigurationMethod = typeof(ProfileExtensions)
-        .GetMethods(BindingFlags.Default | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
-        .Where(m => m.Name == nameof(ApplyConfiguration) && m.IsGenericMethod)
-        .First();
+    static readonly MethodInfo GenericApplyConfigurationMethod = typeof(ProfileExtensions).GetMethods(BindingFlags.Default | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static).Where(m => m.Name == nameof(ApplyConfiguration) && m.IsGenericMethod).First();
 
     /// <summary>
     /// Applies the specified configuration
@@ -33,10 +31,7 @@ public static class ProfileExtensions
     /// <typeparam name="TDestination">The type to map to</typeparam>
     /// <param name="profile">The <see cref="AutoMapper.Profile"/> to configure</param>
     /// <param name="configuration">The <see cref="IMappingConfiguration{TSource, TDestination}"/> to apply</param>
-    public static void ApplyConfiguration<TSource, TDestination>(this AutoMapper.Profile profile, IMappingConfiguration<TSource, TDestination> configuration)
-    {
-        configuration.Configure(profile.CreateMap<TSource, TDestination>());
-    }
+    public static void ApplyConfiguration<TSource, TDestination>(this AutoMapper.Profile profile, IMappingConfiguration<TSource, TDestination> configuration) => configuration.Configure(profile.CreateMap<TSource, TDestination>());
 
     /// <summary>
     /// Applies the specified <see cref="IMappingConfiguration"/>
@@ -52,6 +47,27 @@ public static class ProfileExtensions
             var sourceType = configurationType.GetGenericArguments()[0];
             var destinationType = configurationType.GetGenericArguments()[1];
             GenericApplyConfigurationMethod.MakeGenericMethod(sourceType, destinationType).Invoke(null, new object[] { profile, configuration });
+        }
+    }
+
+    /// <summary>
+    /// Applies all <see cref="IMappingConfiguration"/>s found in the specified assemblies and registers all type maps configured using <see cref="MapAttribute"/>s
+    /// </summary>
+    /// <param name="profile">The extended <see cref="AutoMapper.Profile"/></param>
+    /// <param name="serviceProvider">The current <see cref="IServiceProvider"/></param>
+    /// <param name="assemblies">An array containing the assemblies to search for <see cref="IMappingConfiguration"/>s. If null or empty, will search the calling <see cref="Assembly"/></param>
+    public static void Configure(this AutoMapper.Profile profile, IServiceProvider serviceProvider, params Assembly[] assemblies)
+    {
+        if(assemblies == null || !assemblies.Any()) assemblies = new Assembly[] { Assembly.GetCallingAssembly() };
+        var types = assemblies.SelectMany(a => a.GetTypes());
+        foreach(var mappingConfigurationType in types.Where(t => !t.IsAbstract && !t.IsInterface && t.IsClass && typeof(IMappingConfiguration).IsAssignableFrom(t)))
+        {
+            profile.ApplyConfiguration((IMappingConfiguration)ActivatorUtilities.CreateInstance(serviceProvider, mappingConfigurationType));
+        }
+        foreach(var sourceType in types.Where(t => t.TryGetCustomAttribute<MapAttribute>(out _)))
+        {
+            var mapAttribute = sourceType.GetCustomAttribute<MapAttribute>()!;
+            profile.CreateMap(sourceType, mapAttribute.DestinationType);
         }
     }
 
