f4606977eec3d349e6f5883fa1a194f1ed8ee421
Charles d'Avernas <charles.davernas@neuroglia.io>
2023-11-12 12:56:04 +0100

    feat(Data): Added a Nested Set Model tree implementation

---
+++ src/Neuroglia.Data/NsmTree.cs
@@ -0,0 +1,81 @@
+﻿// Copyright © 2021-Present Neuroglia SRL. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"),
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Neuroglia.Data;
+
+/// <summary>
+/// Represents a <see href="https://en.wikipedia.org/wiki/Nested_set_model">Nested Set Model (NSM)</see> tree
+/// </summary>
+/// <typeparam name="T">The type of data to build a new <see cref="NsmTree{T}"/> for</typeparam>
+public class NsmTree<T>
+{
+
+    protected List<NsmTreeNode<T>>? _rootNodes;
+    /// <summary>
+    /// Gets a list containing the tree's root nodes, if any
+    /// </summary>
+    public virtual IReadOnlyCollection<NsmTreeNode<T>>? RootNodes => this._rootNodes?.AsReadOnly();
+
+    /// <summary>
+    /// Flattens the <see cref="NsmTree{T}"/>
+    /// </summary>
+    /// <returns>A new <see cref="IEnumerable{T}"/> containing all the <see cref="NsmTreeNode{T}"/> the tree is made out of</returns>
+    public virtual IEnumerable<NsmTreeNode<T>>? Flatten() => this.RootNodes?.SelectMany(n => n.Flatten());
+
+    /// <summary>
+    /// Builds a new <see cref="NsmTree{T}"/> using the specified hierarchy root items
+    /// </summary>
+    /// <param name="rootItems">The root items of the hierarchy to build a new <see cref="NsmTree{T}"/> for</param>
+    /// <param name="childrenResolver">A function used to resolve the children of a given item</param>
+    /// <returns>A new <see cref="NsmTree{T}"/></returns>
+    public static NsmTree<T> BuildFor(IEnumerable<T> rootItems, Func<T, IEnumerable<T>> childrenResolver)
+    {
+        if (rootItems == null || !rootItems.Any()) throw new ArgumentNullException(nameof(rootItems));
+        if (childrenResolver == null) throw new ArgumentNullException(nameof(childrenResolver));
+
+        var rootNode = new NsmTreeNode<T>(0, 0, 0, null, default!);
+        Populate(rootNode, rootItems, childrenResolver);
+
+        var tree = new NsmTree<T> { _rootNodes = rootNode.ChildNodes?.ToList() };
+        return tree;
+    }
+
+    /// <summary>
+    /// Populates a <see cref="NsmTreeNode{T}"/> using the specified collection of items
+    /// </summary>
+    /// <param name="parent">The <see cref="NsmTreeNode{T}"/> to populate</param>
+    /// <param name="items">The collection of items to populate the <see cref="NsmTreeNode{T}"/> with</param>
+    /// <param name="childrenResolver">A function used to resolve the children of a given item</param>
+    protected static void Populate(NsmTreeNode<T> parent, IEnumerable<T>? items, Func<T, IEnumerable<T>> childrenResolver)
+    {
+        if (parent == null) throw new ArgumentNullException(nameof(parent));
+        if (childrenResolver == null) throw new ArgumentNullException(nameof(childrenResolver));
+        if (items == null || !items.Any()) return;
+
+        var lastRight = parent.Left;
+        foreach (var item in items)
+        {
+            var left = lastRight + 1;
+            var right = left + 1;
+            var depth = parent.Left == 0 ? 0 : parent.Depth + 1;
+            var lineage = depth == 0 ? null : parent.Lineage + parent.Left.ToString("X8");
+            var node = new NsmTreeNode<T>(left, right, depth, lineage, item, parent);
+            parent.Append(node);
+            Populate(node, childrenResolver.Invoke(item), childrenResolver);
+            lastRight = node.Right;
+        }
+        parent.Freeze();
+
+    }
+
+}
---
+++ test/Neuroglia.UnitTests/Neuroglia.UnitTests/Cases/Data/NsmTreeTests.cs
@@ -0,0 +1,156 @@
+﻿// Copyright © 2021-Present Neuroglia SRL. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"),
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Neuroglia.Data;
+
+namespace Neuroglia.UnitTests.Cases.Data;
+
+public class NsmTreeTests
+{
+
+    [Fact]
+    public void Build_Tree_Should_Work()
+    {
+        //arrange
+        var categories = BuildCategories();
+
+        //act
+        var tree = NsmTree<Category>.BuildFor(categories.Where(c => string.IsNullOrWhiteSpace(c.ParentId)), c => GetChildrenOf(c, categories));
+
+        //assert
+        tree.Should().NotBeNull();
+        tree.RootNodes.Should().ContainSingle();
+    }
+
+    [Fact]
+    public void Flatten_Tree_Should_Work()
+    {
+        //arrange
+        var categories = BuildCategories();
+        var tree = NsmTree<Category>.BuildFor(categories.Where(c => string.IsNullOrWhiteSpace(c.ParentId)), c => GetChildrenOf(c, categories));
+
+        //act
+        var nodes = tree.Flatten();
+
+        //assert
+        nodes.Should().NotBeNull();
+        nodes.Should().HaveSameCount(categories);
+        nodes!.Select(n => n.Value).Should().BeEquivalentTo(categories);
+    }
+
+    [Fact]
+    public void Find_Node_Ancestor_Should_Work()
+    {
+        //arrange
+        var categories = BuildCategories();
+        var tree = NsmTree<Category>.BuildFor(categories.Where(c => string.IsNullOrWhiteSpace(c.ParentId)), c => GetChildrenOf(c, categories));
+        var nodes = tree.Flatten()!;
+        var fishNode = nodes.First(c => c.Value.Id == "fish");
+
+        //act
+        var ancestors = nodes.Where(n => !string.IsNullOrWhiteSpace(n.Lineage) && n.Lineage.StartsWith(fishNode.Lineage![..((int)(fishNode.Depth - 1) * 8)]) && n.Left < fishNode.Left && n.Right > fishNode.Right);
+
+        //assert
+        ancestors.Should().NotBeNull();
+        ancestors.Should().ContainSingle();
+        ancestors.First().Value.Id.Should().Be(fishNode.Value.ParentId);
+    }
+
+    [Fact]
+    public void Find_Node_Ancestors_Should_Work()
+    {
+        //arrange
+        var categories = BuildCategories();
+        var tree = NsmTree<Category>.BuildFor(categories.Where(c => string.IsNullOrWhiteSpace(c.ParentId)), c => GetChildrenOf(c, categories));
+        var nodes = tree.Flatten()!;
+        var node = nodes.First(c => c.Value.Id == "fish");
+
+        //act
+        var ancestors = nodes.Where(n => (string.IsNullOrWhiteSpace(n.Lineage) || n.Lineage.StartsWith(node.Lineage![..8])) && n.Left < node.Left && n.Right > node.Right);
+
+        //assert
+        ancestors.Should().NotBeNull();
+        ancestors.Should().HaveCount((int)node.Depth);
+    }
+
+    [Fact]
+    public void Find_Node_Direct_Descendants_Should_Work()
+    {
+        //arrange
+        var categories = BuildCategories();
+        var tree = NsmTree<Category>.BuildFor(categories.Where(c => string.IsNullOrWhiteSpace(c.ParentId)), c => GetChildrenOf(c, categories));
+        var nodes = tree.Flatten()!;
+        var node = nodes.First(c => c.Value.Id == "food");
+
+        //act
+        var descendants = nodes.Where(n => !string.IsNullOrWhiteSpace(n.Lineage) && n.Lineage.Length / 8 == node.Depth + 1 && n.Left > node.Left && n.Right < node.Right);
+
+        //assert
+        descendants.Should().NotBeNull();
+        descendants.Should().HaveSameCount(categories.Where(c => c.ParentId == node.Value.Id));
+    }
+
+    [Fact]
+    public void Find_Node_All_Descendants_Should_Work()
+    {
+        //arrange
+        var categories = BuildCategories();
+        var tree = NsmTree<Category>.BuildFor(categories.Where(c => string.IsNullOrWhiteSpace(c.ParentId)), c => GetChildrenOf(c, categories));
+        var nodes = tree.Flatten()!;
+        var node = nodes.First(c => c.Value.Id == "food");
+
+        //act
+        var descendants = nodes.Where(n => !string.IsNullOrWhiteSpace(n.Lineage) && n.Lineage.StartsWith(node.Lineage!) && n.Left > node.Left && n.Right < node.Right);
+
+        //assert
+        descendants.Should().NotBeNull();
+    }
+
+    static IEnumerable<Category> GetChildrenOf(Category category, IEnumerable<Category> categories) => categories.Where(c => c.ParentId == category.Id);
+
+    static IEnumerable<Category> BuildCategories()
+    {
+        var products = new Category("products");
+        var drinks = new Category("drinks", products.Id);
+        var soda = new Category("soda", drinks.Id);
+        var juices = new Category("juices", drinks.Id);
+        var freshJuices = new Category("fresh juices", juices.Id);
+        var bottledJuices = new Category("bottled juices", juices.Id);
+        var waters = new Category("waters", drinks.Id);
+        var alcohol = new Category("alcohol", drinks.Id);
+        var food = new Category("food", products.Id);
+        var starters = new Category("starters", food.Id);
+        var mainCourses = new Category("main courses", food.Id);
+        var meat = new Category("meat", mainCourses.Id);
+        var fish = new Category("fish", mainCourses.Id);
+        var vegetarian = new Category("vegetarian", mainCourses.Id);
+        var desserts = new Category("desserts", food.Id);
+        return new List<Category>() { products, drinks, soda, juices, freshJuices, bottledJuices, waters, alcohol, food, starters, mainCourses, meat, fish, vegetarian, desserts };
+    }
+
+    class Category
+    {
+
+        public Category(string id, string? parentId = null)
+        {
+            this.Id = id;
+            this.ParentId = parentId;
+        }
+
+        public string Id { get; }
+
+        public string? ParentId { get; }
+
+    }
+
+}
