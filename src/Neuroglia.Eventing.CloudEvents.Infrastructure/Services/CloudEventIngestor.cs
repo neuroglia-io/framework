7320a876c3a3f257ea30f3cd4faf6602d50fa620
Charles d'Avernas <charles.davernas@neuroglia.io>
2023-11-20 18:50:40 +0100

    Added the CloudEventIngestor class (WIP)

---
+++ src/Neuroglia.Eventing.CloudEvents.Infrastructure/Services/CloudEventIngestor.cs
@@ -0,0 +1,59 @@
+﻿// Copyright © 2021-Present Neuroglia SRL. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"),
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.Extensions.Hosting;
+
+namespace Neuroglia.Eventing.CloudEvents.Infrastructure.Services;
+
+/// <summary>
+/// Represents the service used to ingest incoming <see cref="CloudEvent"/>s
+/// </summary>
+public class CloudEventIngestor
+    : IHostedService, IDisposable
+{
+
+    bool _disposed;
+
+    /// <summary>
+    /// Gets the <see cref="CloudEventIngestor"/>'s <see cref="System.Threading.CancellationTokenSource"/>
+    /// </summary>
+    protected CancellationTokenSource CancellationTokenSource { get; } = new();
+
+    /// <inheritdoc/>
+    public virtual async Task StartAsync(CancellationToken cancellationToken)
+    {
+
+    }
+
+    /// <inheritdoc/>
+    public virtual Task StopAsync(CancellationToken cancellationToken) => Task.Run(this.CancellationTokenSource.Cancel, cancellationToken);
+
+    /// <summary>
+    /// Disposes of the <see cref="CloudEventIngestor"/>
+    /// </summary>
+    /// <param name="disposing">A boolean indicating whether or not the object is being disposed of</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (this._disposed) return;
+        if (disposing) this.CancellationTokenSource.Dispose();
+        this._disposed = true;
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        this.Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+}
