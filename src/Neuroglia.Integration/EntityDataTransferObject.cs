523a1e3cdbd4c339fcbda4ddf39cc936a70c53b1
Charles d'Avernas <charles.davernas@neuroglia.io>
2023-11-23 10:42:46 +0100

    feat(Solution): Added a new Integration project

---
+++ src/Neuroglia.Integration/EntityDataTransferObject.cs
@@ -0,0 +1,51 @@
+﻿// Copyright © 2021-Present Neuroglia SRL. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"),
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Runtime.Serialization;
+
+namespace Neuroglia;
+
+/// <summary>
+/// Represents the base class for Data Transfer Objects used to describe an entity
+/// </summary>
+/// <typeparam name="TKey">The type of key used to identify the described entity</typeparam>
+[DataContract]
+public abstract record EntityDataTransferObject<TKey>
+    : DataTransferObject, IIdentifiable<TKey>
+    where TKey : IEquatable<TKey>
+{
+
+    /// <summary>
+    /// Gets/sets the id of the described entity
+    /// </summary>
+    [DataMember]
+    public virtual TKey Id { get; set; } = default!;
+
+    /// <summary>
+    /// Gets/sets the date and time the described entity has been created at
+    /// </summary>
+    [DataMember]
+    public virtual DateTimeOffset CreatedAt { get; set; }
+
+    /// <summary>
+    /// Gets/sets the date and time the described entity has last been modified
+    /// </summary>
+    [DataMember]
+    public virtual DateTimeOffset? LastModified { get; set; }
+
+    object IIdentifiable.Id => this.Id;
+
+    /// <inheritdoc/>
+    public virtual bool Equals(IIdentifiable<TKey>? other) => other != null && other.GetType() == this.GetType() && this.Id.Equals(this.Id);
+
+}
---
+++ src/Neuroglia.Integration/IntegrationEvent.cs
@@ -0,0 +1,47 @@
+﻿// Copyright © 2021-Present Neuroglia SRL. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"),
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Neuroglia;
+
+/// <summary>
+/// Represents the base class for all integration events
+/// </summary>
+public abstract record IntegrationEvent
+    : DataTransferObject
+{
+
+    /// <summary>
+    /// Gets/sets the id of the aggregate, if any, that has produced the event
+    /// </summary>
+    public virtual object? AggregateId { get; set; }
+
+    /// <summary>
+    /// Gets/sets the date and time at which the integration event has been produced
+    /// </summary>
+    public virtual DateTimeOffset CreatedAt { get; set; }
+
+}
+
+/// <summary>
+/// Represents the base class for all integration events
+/// </summary>
+public abstract record IntegrationEvent<TKey>
+    : IntegrationEvent
+{
+
+    /// <summary>
+    /// Gets/sets the id of the aggregate, if any, that has produced the event
+    /// </summary>
+    public virtual new TKey? AggregateId { get; set; }
+
+}
