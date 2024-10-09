1b69c142a1a57760fd1275e296fd65f63137302e
Charles d'Avernas <charles.davernas@neuroglia.io>
2024-10-09 15:17:03 +0200

    fix(ResourceOriented): Fixed the ResourceAdmissionControl to properly document errors that have occured during admission of evaluated resources
    Signed-off-by: Charles d'Avernas <charles.davernas@neuroglia.io>

--- src/Neuroglia.Data.Infrastructure.ResourceOriented/Services/AdmissionControl.cs
+++ src/Neuroglia.Data.Infrastructure.ResourceOriented/Services/AdmissionControl.cs
@@ -125,7 +125,7 @@ public class AdmissionControl(IServiceProvider serviceProvider, IEnumerable<IPat
 
         var results = tasks.Select(t => t.Result);
         if (results.All(t => t.Allowed)) return new(request.Uid, true);
-        else return new(request.Uid, false, null, new ProblemDetails(ProblemTypes.AdmissionFailed, ProblemTitles.AdmissionFailed, (int)HttpStatusCode.BadRequest, errors: results.Where(r => !r.Allowed && r.Problem != null && r.Problem.Errors != null).SelectMany(r => r.Problem!.Errors!).ToDictionary(e => e.Key, e => e.Value)));
+        else return new(request.Uid, false, null, new ProblemDetails(ProblemTypes.AdmissionFailed, ProblemTitles.AdmissionFailed, (int)HttpStatusCode.BadRequest, errors: results.Where(r => !r.Allowed && r.Problem != null).Select(r => r.Problem!).ToDictionary(p => $"[{p.Status} - {p.Title ?? p.Type?.OriginalString ?? "Unknown"}]", p => new string[] { p.Detail! })));
     }
 
 }
